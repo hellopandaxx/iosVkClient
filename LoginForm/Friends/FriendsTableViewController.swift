58eb89395269c765cd51365af4833e47feddabc2
Sergey Kuznetsov <skuznetsov@SKuznetsov-MBP-SPB.local>
2018-10-26 03:22:58 +0300

    fixed friends list constraints

--- LoginForm/Friends/FriendsTableViewController.swift
+++ LoginForm/Friends/FriendsTableViewController.swift
@@ -75,8 +75,15 @@ class FriendsTableViewController: UITableViewController {
         let friend = friends![indexPath.row]
         cell?.nameLabel.text = friend.firstName + " " + friend.lastName
         
+        cell?.friendImage.image = nil
+        
         // Acync image loading.
         cell?.friendImage.downloadedFrom(link: friend.photo50Url)
+        
+        cell?.friendImage.layer.cornerRadius = (cell?.friendImage.bounds.height ?? 0) / 2.0
+        cell?.friendImage.layer.masksToBounds = true
+        
+        //cell?.bounds.size height = (cell?.friendImage.bounds.height ?? 0) + 10
 
         return cell!
     }
@@ -92,49 +99,4 @@ class FriendsTableViewController: UITableViewController {
             }
         }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
--- LoginForm/MyGroups/MyGroupsController.swift
+++ LoginForm/MyGroups/MyGroupsController.swift
@@ -61,7 +61,7 @@ class MyGroupsController: UITableViewController {
 
          if let indexPath = allGroupsController.allGroupsTable.indexPathForSelectedRow {
              let newGroup = allGroupsController.allGroups[indexPath.row]
-             // let groupName = newGroup.name
+            
              if !newGroup.isMember{
                 vkService.joinGroup(id: newGroup.id){[weak self] result in
                     if(result){
@@ -72,15 +72,6 @@ class MyGroupsController: UITableViewController {
          }
      }
     }
- 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -103,30 +94,4 @@ class MyGroupsController: UITableViewController {
             self?.myGroupsTable.reloadData()
         }
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
